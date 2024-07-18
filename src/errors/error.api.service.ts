75a4e5b81657473d241d1a7893dcd24ce6736e29
Olegogonovskyi <Olegogonowskyi@gmail.com>
2024-07-18 10:09:54 +0300

    main: clean branch

--- src/errors/error.api.service.ts
+++
@@ -1,6 +0,0 @@
-export class ErrorApiService extends Error {
-    constructor(public message: string, public status: number) {
-        super(message);
-        this.status = status
-    }
-}
--- src/route/users.route.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import {userController} from "../contollers/userController";
-
-
-const route = Router()
-
-route.get('/', userController.getAll)
-
-export const userRouter = route
