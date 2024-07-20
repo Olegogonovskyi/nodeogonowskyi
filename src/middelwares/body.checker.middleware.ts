af191e2ab3df0aee62c00c5b38eec2cd3768bda4
Olegogonovskyi <Olegogonowskyi@gmail.com>
2024-07-20 09:51:05 +0300

    train-mg: hw5 done

--- src/middelwares/body.checker.middleware.ts
+++ src/middelwares/body.checker.middleware.ts
@@ -3,15 +3,31 @@ import {isValidName} from "../patterns/pattern.service";
 import {ApiErrors} from "../errors/error.api.service";
 
 class BodyCheckerMiddleware {
-    public isBodyValid() {
+    public isBodyValid(idParams: boolean) {
         return (req: Request, res: Response, next: NextFunction) => {
+            const {name, superpowers, age} = req.body
             try {
-                if (!isValidName(req.body.name, /^[a-zA-Zа-яА-Я\s]{3,}$/)) {
+                if (idParams) {
+                    const id = req.body.id
+                    if (req.body._id != id) {
+                        throw new ApiErrors('ID never change', 400)
+                    }
+                }
+                if (!isValidName(name, /^[a-zA-Zа-яА-Я\s]{2,}$/)) {
                     throw new ApiErrors('Somesing wrong with your name', 400)
                 }
+                if (!superpowers.length) {
+                    throw new ApiErrors('It is not superhero', 400)
+                }
+                if (!age || age < 1) {
+                    throw new ApiErrors('Go to bad little child', 400)
+                }
+                next()
             } catch (e) {
-
+                next(e)
             }
         }
     }
 }
+
+export const bodyCheckerMiddleware = new BodyCheckerMiddleware()
--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,14 +1,15 @@
 import {Router} from "express";
 import {userControllers} from "../controllers/userControllers";
 import {idChekkerMiddleware} from "../middelwares/id.chekker.middleware";
+import {bodyCheckerMiddleware} from "../middelwares/body.checker.middleware";
 
 const router = Router()
 
 router.get('/', userControllers.getAll)
-router.post('/', userControllers.create)
+router.post('/', bodyCheckerMiddleware.isBodyValid(false), userControllers.create)
 router.get('/:id', idChekkerMiddleware.isIdValid('id'), userControllers.getById)
 router.delete('/:id', idChekkerMiddleware.isIdValid('id'), userControllers.deleteUser)
-router.put('/:id', idChekkerMiddleware.isIdValid('id'), userControllers.put)
+router.put('/:id', idChekkerMiddleware.isIdValid('id'), bodyCheckerMiddleware.isBodyValid(true), userControllers.put)
 
 
 export const userRouter = router
