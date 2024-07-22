9c67b2e25be39cc7d6fb0f8694dbc723fd9ddb19
Olegogonovskyi <Olegogonowskyi@gmail.com>
2024-07-22 16:45:00 +0300

    train-valid-auth: prew

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -4,7 +4,8 @@ import {IUser} from "../intwrfaces/IUser";
 const shema = new Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
